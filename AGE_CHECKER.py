7266733a6d761f4c4506ee3d17a1a5097dc01fe0
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-12-08 10:23:20 +0530

    first commit

---
+++ AGE_CHECKER.py
@@ -0,0 +1,8 @@
+# creating the algorithm
+
+num = int(input("Enter your age: "))
+
+if (num >= 10 and num <= 20):
+    print("You are eligible for joing the class.")
+else:
+    print("You are not eligible for joing the class.")
